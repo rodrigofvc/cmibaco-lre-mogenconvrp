1bd700f493e83e8479e0c4719e4cc4ff86c84041
RodrigoVelazquez99 <rodrigo_velazquez99@ciencias.unam.mx>
2023-05-11 01:20:40 -0600

    improve for costumer

--- src/costumer.py
+++ src/costumer.py
@@ -1,3 +1,5 @@
+import math
+
 class Costumer(object):
 
     def __init__(self, id, x, y, demands=[], service_times=[]):
@@ -11,10 +13,25 @@ class Costumer(object):
         # [(day, service_time),....] how long takes the service per day
         self.service_times = service_times
         # [(day, arrival_time),...] at wich hour arrived the vehicle at costumer ---- TODO
-        self.arrival_times = []
+        self.arrival_times = [-1] * len(self.demands)
+        # drivers that visited costumer
+        self.vehicles_visit = [-1] * len(self.demands)
 
-    def get_distance(self, other):
-        return -1
+    def distance_to(self, other):
+        return math.sqrt(math.pow(self.x - other.x, 2) + math.pow(self.y - other.y, 2))
+
+    def get_max_arrival_diference(self):
+        arrivals = [a for a in self.arrival_times if a > -1]
+        max_diff = 0
+        for a in arrivals:
+            for a_ in arrivals:
+                max_diff = max(max_diff, abs(a - a_))
+        return max_diff
+
+    def get_max_vehicle_difference(self):
+        vehicles = [v for v in self.vehicles_visit if v > -1]
+        max_diff = len(list(set(vehicles)))
+        return max_diff
 
     def __eq__(self, other):
         return self.id == other.id
@@ -27,4 +44,5 @@ class Costumer(object):
         to_string += 'demands: ' + str(self.demands) + ',\n'
         to_string += 'service_times: ' + str(self.service_times) + ',\n'
         to_string += 'arrival_times: ' + str(self.arrival_times) + ',\n'
+        to_string += 'vehicles_visit: ' + str(self.vehicles_visit) + ',\n'
         return to_string
