3e5f137d4ed11766ad8661425b870c784dbe9b07
RodrigoVelazquez99 <rodrigo_velazquez99@ciencias.unam.mx>
2023-05-11 01:21:17 -0600

    improvement in vehicle

--- src/vehicle.py
+++ src/vehicle.py
@@ -1,13 +1,76 @@
 class Vehicle():
 
-    def __init__(self, id, capacity):
+    def __init__(self, id, capacity, days):
         self.id = id
         self.capacity = capacity
+        self.loads = [0] * days
         self.tour = {}
 
     def set_tour_day(self, day, tour):
         self.tour[day] = tour
 
+    def add_costumer_tour_day(self, day, costumer_j):
+        costumer_i = self.tour[day][-1]
+        if costumer_j.service_times[day] <= 0:
+            raise()
+        if costumer_i.arrival_times[day] <= 0 and costumer_i.id != 0:
+            raise()
+        arrival_j = costumer_i.arrival_times[day]
+        arrival_j += costumer_i.service_times[day]
+        arrival_j += costumer_i.distance_to(costumer_j)
+        costumer_j.arrival_times[day] = arrival_j
+        self.tour[day].append(costumer_j)
+        self.loads[day] += costumer_j.demands[day]
+        costumer_j.vehicles_visit[day] = self.id
+        self.check_load(day)
 
-    def get_time(self):
-        return 0
+    def check_load(self, day):
+        if self.loads[day] > self.capacity:
+            raise(f'Vehicle {self.id} overfull')
+
+    def get_time(self, day):
+        time = 0
+        if not day in self.tour.keys():
+            return time
+        tour_day = self.tour[day]
+        for i in range(len(tour_day)):
+            costumer_1 = tour_day[i]
+            j = i+1
+            if j >= len(tour_day):
+                break
+            costumer_2 = tour_day[j]
+            time += costumer_1.distance_to(costumer_2)
+        time += tour_day[0].distance_to(tour_day[-1])
+        return time
+
+    def visited_costumer(self, costumer_j):
+        days = self.tour.keys()
+        for d in days:
+            tour_day = self.tour[d]
+            if costumer_j in tour_day:
+                return True
+        return False
+
+    """
+    def set_arrive_time_costumers(self):
+        days = self.tour.keys()
+        for d in days:
+            self.set_arrive_time_costumers(d)
+
+    def set_arrive_time_costumers(self, day):
+        tour_day = self.tour[day]
+        depot = tour_day[0]
+        costumer_1 = tour_day[1]
+        time = depot.distance_to(costumer_1)
+        costumer_1.set_arrive_time(time)
+        for i in range(1,len(tour_day)):
+            costumer_1 = tour_day[i]
+            j = i+1
+            if j >= len(tour_day):
+                break
+            costumer_2 = tour_day[j]
+            arrive_time_2 = costumer_1.arrival_times[day]
+            arrive_time_2 += costumer_1.service_times[day]
+            arrive_time_2 += costumer_1.distance_to(costumer_2)
+            costumer_2.set_arrive_time(arrive_time_2)
+    """
