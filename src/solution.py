d224bff514d3db4a7607aaf9256d6f87e7f51f55
RodrigoVelazquez99 <rodrigo_velazquez99@ciencias.unam.mx>
2023-05-17 23:55:24 -0600

    improvement for solution

--- src/solution.py
+++ src/solution.py
@@ -6,6 +6,10 @@ class Solution:
         self.timetables = timetables
         self.assigments_vehicles = {'AM': [], 'PM': []}
         self.assigments_costumers = {'AM': [], 'PM': []}
+        self.f_1 = None
+        self.f_2 = None
+        self.f_3 = None
+        self.f_4 = None
 
     def add_assigment_vehicles(self, vehicles, costumers, timetable):
         self.assigments_vehicles[timetable] = vehicles
@@ -26,6 +30,26 @@ class Solution:
                     max_diff = max(max_diff, costumer.get_max_arrival_diference())
         return max_diff
 
+    def max_costumer(self):
+        c = None
+        max_diff = 0
+        for timetable in self.timetables:
+            for costumer in self.assigments_costumers[timetable]:
+                    m = costumer.get_max_arrival_diference()
+                    if m >= max_diff:
+                        c = costumer
+                        max_diff = m
+        return c
+
+    def get_mean_arrive_difference(self):
+        diff = 0
+        n = 0
+        for timetable in self.timetables:
+            n += len(self.assigments_costumers[timetable])
+            for costumer in self.assigments_costumers[timetable]:
+                diff += costumer.get_max_arrival_diference()
+        return diff / n
+
     def get_max_difference_drivers(self):
         max_driver_diff = 0
         for timetable in self.timetables:
@@ -34,14 +58,27 @@ class Solution:
         return max_driver_diff
 
     def get_fitness(self):
-        f_1 = self.get_total_time()
-        f_2 = self.get_max_difference_arrive()
-        f_3 = self.get_max_difference_drivers()
-        return (f_1, f_2, f_3)
+        self.f_1 = self.get_total_time()
+        self.f_2 = self.get_max_difference_arrive()
+        self.f_3 = self.get_max_difference_drivers()
+        self.f_4 = self.get_mean_arrive_difference()
+        return (self.f_1, self.f_2, self.f_3)
 
     def dominates(self, y):
-        f_1, f_2, f_3 = self.get_fitness()
-        f_1y, f_2y, f_3y = y.get_fitness()
-        if f_1 <= f_1y and f_2 <= f_2y and f_3 <= f_3y:
+        # F(X) == F(Y)
+        if abs(self.f_1 - y.f_1) <= 10e-8 and abs(self.f_2 - y.f_2) <= 10e-8 and self.f_3 == y.f_3:
+            return False
+        # F(X) <= F(Y)
+        if abs(self.f_1 - y.f_1) <= 10e-8 and abs(self.f_2 - y.f_2) <= 10e-8 and self.f_3 <= y.f_3:
             return True
         return False
+
+    def is_feasible(self):
+        for timetable in self.timetables:
+            vehicles = self.assigments_vehicles[timetable]
+            for vehicle in vehicles:
+                vehicle.is_feasible()
+            customers = self.assigments_costumers[timetable]
+            for customer in customers:
+                customer.is_feasible()
+        return True
