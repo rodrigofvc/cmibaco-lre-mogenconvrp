99cd8036a7c6357256e78616af1bf5e92cb2ec28
RodrigoVelazquez99 <rodrigo_velazquez99@ciencias.unam.mx>
2023-08-02 13:30:10 -0600

    deleted files

--- src/arc.py
+++
@@ -1,6 +0,0 @@
-class Arc():
-
-    def __init__(self, costumer_1, costumer_2, time):
-        self.costumer_1 = costumer_1
-        self.costumer_2 = costumer_2
-        self.time = time
--- src/mogenconvrp.py
+++
@@ -1,9 +0,0 @@
-class MOGenConVRP():
-
-    def __init__(self, days, costumers):
-        # days for planning horizon
-        self.days = days
-        # FREE
-        self.vehicles = vehicles
-        # Nodes of graph
-        self.costumers = costumers
